877d833bdbd61f42cd34e509eefa8f86730aacfb
Vinicius Vacare <viniciusvacare@protonmail.com>
2023-08-15 23:01:30 -0300

    commit 15/08

--- main.go
+++ main.go
@@ -3,7 +3,16 @@ package main
 import "github.com/gin-gonic/gin"
 
 func main() {
+	// inicializa o router utilizando as config default do gin
 	r := gin.Default()
 
-	print(r)
+	// define uma rota
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "pong",
+		})
+	})
+
+	// executa a api
+	r.Run()
 }
